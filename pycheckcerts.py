9390a5cb6e4dd1a55bb4c633586a412aac480d4c
Max J Rodriguez Beltran <max@sml.mx>
2018-09-18 16:32:58 -0600

    added arg to check url instead of reading hosts file

--- pycheckcerts.py
+++ pycheckcerts.py
@@ -22,16 +22,26 @@ filehosts = os.path.join(my_path,"Hosts")
 daysToCheck = 3 #Change it to the days you want to set to send mail to warn you about expire day
 #######################
 
-if not os.path.isfile(filehosts):
-    print("File Hosts doesn't exist...")
-    exit(2)
-
-with open(filehosts, mode='r') as hostsfile:
-    listHosts = [(line.strip()).replace("\n","") for line in hostsfile if '#' not in line]
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--resume", "-r", help="resume all hosts in 1 mail", action="store_true")
+parser.add_argument("--url", "-u", help="pass host to check for expiry date", action="append")
 args = parser.parse_args()
+
+def send_mail(content):
+
+    message = MIMEMultipart()
+    # Change to the mail that you want to send
+    message['Subject'] = "Certs close to expire"
+    message['From'] = "certchecker@yourmail.com"
+    message['To'] = "yourmail@mail.com, othermail@othermail.com"
+
+    body = MIMEText(content, 'html')
+    message.attach(body)
+
+    with smtplib.SMTP_SSL('smtp.powweb.com',465) as server:
+        server.login('certchecker@yourmail.com',"YourSecurePassw0rd")
+        server.send_message(message)
+        server.quit()
 
 def checkCert(hostname):
     days_to_expire = 0
@@ -51,21 +61,42 @@ def checkCert(hostname):
 
     return  days_to_expire
 
-def send_mail(content):
+def checkHosts (hostsList, checkDays = False):
+    for host in hostsList:
+        daysToExpire = checkCert(host.strip())
+        message=f"""<!DOCTYPE html>
+            <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <title></title>
+            <meta name="description" content="">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+        </head>
+        <body>
+            <div style="position: relative; text-align: center; color: white;">
+                <img src="https://i.imgur.com/SxzZBDF.png" alt="xpmessage" style="width:40%;">
+                <div style="position: absolute; top: 45%; left:50%; transform: translate(-50%, -50%); font-size: 19px; font-family: Tahoma, Geneva, Verdana, sans-serif; color: rgba(55, 63, 71, 0.822)"><p>{host}</p>En {daysToExpire} días!
+                </div>
+            </div>
+        </body>
+        </html>"""
+        if checkDays:
+            if daysToExpire <= daysToCheck:
+                send_mail(message)
+        else:
+            send_mail(message)
 
-    message = MIMEMultipart()
-    # Change to the mail that you want to send
-    message['Subject'] = "Certs close to expire"
-    message['From'] = "certchecker@yourmail.com"
-    message['To'] = "yourmail@mail.com, othermail@othermail.com"
+#If --url arg is passed, will check for every host is passed as argument and will send one mail per host
+if args.url:
+    checkHosts(args.url)
+    exit(0)
 
-    body = MIMEText(content, 'html')
-    message.attach(body)
+if not os.path.isfile(filehosts):
+    print("File Hosts doesn't exist...")
+    exit(2)
 
-    with smtplib.SMTP_SSL('smtp.powweb.com',465) as server:
-        server.login('certchecker@yourmail.com',"YourSecurePassw0rd")
-        server.send_message(message)
-        server.quit()
+with open(filehosts, mode='r') as hostsfile:
+    listHosts = [(line.strip()).replace("\n","") for line in hostsfile if '#' not in line]
 
 #If --resume arg is passed, will check for every host in Hosts file and will send them all listed in one mail
 if args.resume :
@@ -91,27 +122,4 @@ if args.resume :
     exit(0)
 
 if listHosts:
-    for host in listHosts:
-        daysToExpire = checkCert(host)
-        if daysToExpire <= daysToCheck:
-            message=f"""<!DOCTYPE html>
-        <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-        <div style="position: relative; text-align: center; color: white;">
-            <!-- this was an example image, you can use any image you want -->
-            <img src="https://i.imgur.com/SxzZBDF.png" alt="xpmessage" style="width:40%;">
-            <div style="position: absolute; top: 45%; left:50%; transform: translate(-50%, -50%); font-size: 19px; font-family: Tahoma, Geneva, Verdana, sans-serif; color: rgba(55, 63, 71, 0.822)"><p>{host}</p>in {daysToExpire} days!
-            </div>
-        </div>
-    </body>
-    </html>"""
-            send_mail(message)
+    checkHosts(listHosts, True)
